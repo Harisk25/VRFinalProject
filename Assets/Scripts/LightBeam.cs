878027af4009f1903330bf6a4aa0a30b0a3d7820
Harisk25 <kajtazovichkmail@gmail.com>
2024-09-18 15:43:21 -0600

    Light ray working
    added and finished light ray interaction

---
+++ Assets/Scripts/LightBeam.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBeam : MonoBehaviour
+{
+
+    [Header("Light Beam Settings")]
+    public LayerMask layerMaskReflect;
+    public LayerMask layerMaskNoReflect;
+    public float defualtLength = 50;
+    public int numOfReflection = 10;
+
+    private LineRenderer lineRenderer;
+    private RaycastHit hit;
+
+    private Ray ray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReflectlLight();
+    }
+
+    void ReflectlLight()
+    {
+        ray = new Ray(transform.position, transform.forward); // Create new ray from start point object
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+        float remainLenght = defualtLength;
+
+        for (int i = 0; i < numOfReflection; i++)
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, remainLenght, layerMaskReflect))
+            {
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition(lineRenderer.positionCount-1, hit.point);
+                remainLenght -= Vector3.Distance(ray.origin, hit.point);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+            }
+            else
+            {
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + (ray.direction * remainLenght));
+            }
+            
+        }
+    }
+    void NormalLight()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, defualtLength, layerMaskNoReflect))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.position + (transform.forward * defualtLength));
+        }
+    }
+}

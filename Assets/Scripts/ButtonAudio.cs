675b8f683f1a13624acdd8b1c94c97f82a18aff8
BeastlyOrca <118201727+BeastlyOrca@users.noreply.github.com>
2024-10-22 14:40:12 -0600

    fixing some audio and tutorial iimprovements

--- Assets/Scripts/ButtonAudio.cs
+++ Assets/Scripts/ButtonAudio.cs
@@ -9,12 +9,42 @@ public class ButtonAudio : MonoBehaviour
     public AudioSource audioSource;    // Reference to the AudioSource component
     public AudioClip[] audioClips;     // Array to hold your audio clips
 
-    private int currentClipIndex = 0;  // To track which clip to play
+    private int currentClipIndex = 1;  // To track which clip to play
+
+        // Reference to your UI button
+    public Button prevButton;
+
+    // Method to be called when the previous button is clicked
+    void OnButtonClick()
+    {
+
+        // Add your custom action here, e.g., load a scene, trigger animation, etc.
+        Debug.Log(currentClipIndex);
+
+        // THE ISSUE IS BECAUSE THE WELCOME IS  NOT IN ARRAY, SO INDEX 0 IS ACTUALLY INSTRUCT1 
+        // SIMPLE FIX IS TO PUT WELCOME AT 0 AND START ARRAY AT 1
+        currentClipIndex--; // its not subtracting here
+        Debug.Log(currentClipIndex);
+            
+        StartCoroutine(PlayAudioSequence());
+            
+        
+        
+
+    }
 
     void Start()
     {
         // Add a listener to the button that will call the PlayNextAudio method when clicked
         playButton.onClick.AddListener(PlayNextAudio);
+
+        // Ensure the button is assigned
+        if (prevButton != null)
+        {
+            // Add a listener to the button's onClick event
+            prevButton.onClick.AddListener(OnButtonClick);
+        }
+
     }
 
     void PlayNextAudio()
@@ -31,6 +61,7 @@ public class ButtonAudio : MonoBehaviour
         // Set the current audio clip to the AudioSource
         audioSource.clip = audioClips[currentClipIndex];
         audioSource.Play();
+
         // Move to the next clip in the array
         currentClipIndex++;
 
